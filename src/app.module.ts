2060dd372ece8febcb051cfd5f238581387b4a36
Nik720 <ndbeladiya720@gmail.com>
2021-09-07 12:07:28 +0530

    Added env config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,19 @@
 import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { config } from './config/config';
 import { SocketGateway } from './socket/socket.gateway';
 
 @Module({
-  imports: [HttpModule],
+  imports: [
+    HttpModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [config],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService, SocketGateway],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { RedisIoAdapter } from './adapters/redis-io.adapter';
 import { AppModule } from './app.module';
+import { config } from './config/config';
 
 async function bootstrap() {
+  const appConfig = config().app;
   const app = await NestFactory.create(AppModule);
   app.useWebSocketAdapter(new RedisIoAdapter(app));
-  await app.listen(3005);
+  await app.listen(appConfig.port).then(() => console.log(`App is running on ${appConfig.port}`));
 }
 bootstrap();
