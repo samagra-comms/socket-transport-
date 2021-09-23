a1dd035eac7d1d0e546bae0f476370f1831916ab
Nik720 <ndbeladiya720@gmail.com>
2021-09-23 12:32:48 +0530

    Print logs in log file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,9 @@
 import { HttpModule } from '@nestjs/axios';
-import { Module } from '@nestjs/common';
+import { Logger, Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import {  utilities as nestWinstonModuleUtilities, WinstonModule } from 'nest-winston';
+import path from 'path';
+import * as winston from 'winston';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { config } from './config/config';
@@ -8,6 +11,37 @@ import { SocketGateway } from './socket/socket.gateway';
 
 @Module({
   imports: [
+    WinstonModule.forRootAsync({
+      useFactory: () => ({
+        format: winston.format.combine(
+            winston.format.timestamp({
+              format: 'YYYY-MM-DD HH:mm:ss'
+            }),
+            winston.format.printf(info => `${info.timestamp} ${info.level}: ${info.message}`+(info.splat!==undefined?`${info.splat}`:" "))
+        ),
+        transports: [
+          new winston.transports.Console({
+            format: nestWinstonModuleUtilities.format.nestLike(),
+          }),
+          new winston.transports.File({
+            dirname: 'log',
+            filename: 'debug.log',
+            level: 'debug',
+          }),
+          new winston.transports.File({
+            dirname: 'log',
+            filename: 'info.log',
+            level: 'info',
+          }),
+          new winston.transports.File({
+            dirname: 'log',
+            filename: 'error.log',
+            level: 'error',
+          }),
+        ],
+      }),
+      inject: [],
+    }),
     HttpModule,
     ConfigModule.forRoot({
       isGlobal: true,
@@ -15,6 +49,7 @@ import { SocketGateway } from './socket/socket.gateway';
     }),
   ],
   controllers: [AppController],
-  providers: [AppService, SocketGateway],
+  providers: [AppService, SocketGateway, Logger],
+  exports: [SocketGateway]
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
 import { RedisIoAdapter } from './adapters/redis-io.adapter';
 import { AppModule } from './app.module';
 import { config } from './config/config';
@@ -7,6 +8,7 @@ import { config } from './config/config';
 async function bootstrap() {
   const appConfig = config().app;
   const app = await NestFactory.create(AppModule);
+  app.useLogger(app.get(WINSTON_MODULE_NEST_PROVIDER));
   app.useWebSocketAdapter(new RedisIoAdapter(app));
   await app.listen(appConfig.port).then(() => console.log(`App is running on ${appConfig.port}`));
 }
