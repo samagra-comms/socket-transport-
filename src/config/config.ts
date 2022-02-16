5968b617bbc836f19e8c5fe828675428725bc197
ChakshuGautam <chaks.gautam@gmail.com>
2022-02-16 18:35:30 +0530

    Feature: UCI-PWA related changes

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,7 +1,7 @@
 export const config = () => ({
   app: {
-    redisport: process.env.REDIS_PORT,
-    redishost: process.env.REDIS_HOST,
+    redisPort: process.env.REDIS_PORT,
+    redisHost: process.env.REDIS_HOST,
     port: process.env.SERVER_PORT,
     bot_session_event: process.env.SKT_SESSION_EVT || 'session',
     socket_timeout: process.env.SKT_TIMEOUT || 60000,
