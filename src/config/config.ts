6bcab452932ee3487a1b656e8020020dfaf3f152
ChakshuGautam <chaks.gautam@gmail.com>
2022-03-09 11:57:33 +0530

    Fix: Port - add default value

--- src/config/config.ts
+++ src/config/config.ts
@@ -2,7 +2,7 @@ export const config = () => ({
   app: {
     redisPort: process.env.REDIS_PORT,
     redisHost: process.env.REDIS_HOST,
-    port: process.env.SERVER_PORT,
+    port: process.env.SERVER_PORT || 3005,
     bot_session_event: process.env.SKT_SESSION_EVT || 'session',
     socket_timeout: process.env.SKT_TIMEOUT || 60000,
     socket_ping_interval: process.env.SKT_PING_INTERVAL || 60000,
