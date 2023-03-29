20649eb919b3079248478d7309b2493d9614be83
Chakshu Gautam <chaks.gautam@gmail.com>
2023-03-29 15:54:27 +0530

    Update ws.guard.ts

--- src/guard/ws.guard.ts
+++ src/guard/ws.guard.ts
@@ -5,12 +5,13 @@ import * as jwt from 'jsonwebtoken';
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const jwksClient = require('jwks-rsa');
 
-const getKey = (header, callback) => {
-  const client = jwksClient({
+const client = jwksClient({
     jwksUri: 'http://103.154.251.109:9011/.well-known/jwks.json',
     requestHeaders: {}, // Optional
     timeout: 30000, // Defaults to 30s
-  });
+});
+
+const getKey = (header, callback) => {
   client.getSigningKey(header.kid, function (err, key) {
     const signingKey = key.publicKey || key.rsaPublicKey;
     callback(null, signingKey);
